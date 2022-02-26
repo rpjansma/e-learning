54631b7ebd181529602768fefa9e9df6fad13bb9
Raul Jansma Paes <rpjansma@gmail.com>
2022-02-26 11:51:11 -0300

    Fixing test.

--- src/test/java/com/elearning/web/exception/handler/RestControllerAdviceHandlerTest.java
+++ src/test/java/com/elearning/web/exception/handler/RestControllerAdviceHandlerTest.java
@@ -2,35 +2,39 @@ package com.elearning.web.exception.handler;
 
 import com.elearning.dto.ErrorDetail;
 import com.elearning.web.exception.exceptions.InternalServerErrorException;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class RestControllerAdviceHandlerTest {
 
-    RestControllerAdviceHandler adviceHandler;
-    InternalServerErrorException internalServerErrorException;
+    RestControllerAdviceHandler adviceHandler = new RestControllerAdviceHandler();
+    WebRequest request = mock(WebRequest.class);
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-
-        adviceHandler = new RestControllerAdviceHandler();
     }
 
     @Test
     void handleInternalServerError() {
         InternalServerErrorException ex = new InternalServerErrorException("Teste");
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorDetail apiError = ErrorDetail.builder().requestDateTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))).code(status.toString()).title(ex.getMessage()).detail(ex.getCause().toString()).build();
-        adviceHandler.handleInternalServerError(ex, apiError, new HttpHeaders(), status, request);
+        ResponseEntity<Object> response = adviceHandler.handleInternalServerError(ex, request);
+        testHandleException(response, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
     @Test
@@ -39,5 +43,11 @@ class RestControllerAdviceHandlerTest {
 
     @Test
     void handleUncaught() {
+    }
+
+    private void testHandleException(ResponseEntity<Object> response, HttpStatus status, String detailMessage) {
+        ErrorDetail errorDetail = (ErrorDetail) response.getBody();
+        assertEquals(response.getStatusCode(), Matchers.equalTo(status));
+        assertEquals(errorDetail.getDetail(), Matchers.equalTo(detailMessage));
     }
 }
